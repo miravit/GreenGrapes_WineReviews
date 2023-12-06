032a1423c72cc02ea134b38d53df3a2315a43229
miravitsas <86630265+miravit@users.noreply.github.com>
2023-12-06 15:02:28 +0100

    feat: add timestamps

--- backend/src/models/Review.ts
+++ backend/src/models/Review.ts
@@ -14,18 +14,21 @@ export interface IReview {
   comment: string;
 }
 
-export const reviewSchema = new Schema<IReview>({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: false },
-  wineName: { type: String, required: true },
-  photo: { type: String, required: true },
-  producer: { type: String, required: true },
-  percentage: { type: String, required: true },
-  price: { type: Number, required: true },
-  rating: { type: Number, required: true },
-  foodPairing: { type: String, required: false },
-  grape: { type: String, required: true },
-  comment: { type: String, required: false },
-});
+export const reviewSchema = new Schema<IReview>(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: false },
+    wineName: { type: String, required: true },
+    photo: { type: String, required: true },
+    producer: { type: String, required: true },
+    percentage: { type: String, required: true },
+    price: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    foodPairing: { type: String, required: false },
+    grape: { type: String, required: true },
+    comment: { type: String, required: false },
+  },
+  { timestamps: true }
+);
 
 export const Review = model<IReview>("Review", reviewSchema);
