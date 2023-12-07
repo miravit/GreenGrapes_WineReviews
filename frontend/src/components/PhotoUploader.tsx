2b9f2b2ccbb737d05beaebbd80bdb55eba7e1c87
miravitsas <86630265+miravit@users.noreply.github.com>
2023-12-07 15:46:36 +0100

    feat : add get request

--- frontend/src/components/PhotoUploader.tsx
+++ frontend/src/components/PhotoUploader.tsx
@@ -1,6 +1,6 @@
 import { MdOutlineAddAPhoto } from "react-icons/md";
 import axios from "axios";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { IReview } from "../models/IReview";
 
 const PhotoUploader = () => {
@@ -33,23 +33,21 @@ const PhotoUploader = () => {
     }
   };
 
-  //console.log(newReview);
-
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append("firstname", newReview.firstname);
-    formData.append("lastname", newReview.lastname);
-    formData.append("wineName", newReview.wineName);
-    formData.append("photo", newReview.photo);
-    formData.append("producer", newReview.producer);
-    formData.append("percentage", newReview.percentage);
-    formData.append("price", newReview.price.toString());
-    formData.append("rating", newReview.rating.toString());
-    formData.append("foodPairing", newReview.foodPairing);
-    formData.append("grape", newReview.grape);
-    formData.append("comment", newReview.comment);
+    formData.append("firstname", newReview.firstname || "");
+    formData.append("lastname", newReview.lastname || "");
+    formData.append("wineName", newReview.wineName || "");
+    formData.append("photo", newReview.photo || "");
+    formData.append("producer", newReview.producer || "");
+    formData.append("percentage", newReview.percentage || "");
+    formData.append("price", newReview.price.toString() || "");
+    formData.append("rating", newReview.rating.toString() || "");
+    formData.append("foodPairing", newReview.foodPairing || "");
+    formData.append("grape", newReview.grape || "");
+    formData.append("comment", newReview.comment || "");
     console.log(FormData);
 
     console.log(formData);
@@ -61,9 +59,33 @@ const PhotoUploader = () => {
     return response.data;
   };
 
+  useEffect(() => {
+    const response = async () => {
+      try {
+        const response = await axios.get<IReview>(
+          "http://localhost:4000/api/v1/review"
+        );
+        console.log(response.data);
+        // Handle the data as needed
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    response();
+  }, []);
+
   return (
     <>
       <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <input
+          type="text"
+          placeholder="firstname"
+          name="firstname"
+          value={newReview.firstname}
+          onChange={handleChange}
+        />
+        <input type="submit" />
         <label className="uploadButton" htmlFor="file_picker">
           <MdOutlineAddAPhoto />
           <input
@@ -75,15 +97,6 @@ const PhotoUploader = () => {
             onChange={(e) => handlePhoto(e)}
           />
         </label>
-
-        <input
-          type="text"
-          placeholder="firstname"
-          name="firstname"
-          value={newReview.firstname}
-          onChange={handleChange}
-        />
-        <input type="submit" />
       </form>
       <p>{photoUploaded}</p>
     </>
