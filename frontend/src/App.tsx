f79e3e1ecd6d666f862f1f9acd43d25c48fe5748
miravitsas <86630265+miravit@users.noreply.github.com>
2023-12-11 13:46:39 +0100

    feat : remov cloudinary component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import AllPhotos from "./components/AllPhotos";
-import CloudinaryPhotos from "./components/CloudinaryPhotos";
 import PhotoUploader from "./components/PhotoUploader";
 
 function App() {
   return (
     <>
       <PhotoUploader />
-      {/* <CloudinaryPhotos /> */}
       <AllPhotos />
     </>
   );
