dc2690ce2971f4be71ecfe5a84b6913174d5985f
miravitsas <86630265+miravit@users.noreply.github.com>
2023-12-20 09:08:39 +0100

    feat: fixes

--- frontend/src/components/pages/ReviewPage.tsx
+++ frontend/src/components/pages/ReviewPage.tsx
@@ -1,19 +1,44 @@
-import { useEffect, useState } from "react";
-import { IReviewContext, ReviewContext } from "../../contexts/ReviewContext";
+import { useEffect, useReducer, useState } from "react";
+import {
+  IReviewContext,
+  ReviewContext,
+  ReviewReducerContext,
+} from "../../contexts/ReviewContext";
 import { IName, IReview } from "../../models/IReview";
 import { createNewReview } from "../../services/reviewApi";
 import Form from "../Form";
 import WelcomePage from "./WelcomePage";
 import { Link } from "react-router-dom";
+import { NewReviewReducer } from "../../reducers/ReviewsReducer";
+import { ReviewDispatchContext } from "../../contexts/ReviewDispatchContext";
 
 export const ReviewPage = () => {
   const [isFormVisible, setIsFormVisible] = useState(false);
 
+  //with this state we create the review
+  const [createReview, dispatch] = useReducer(NewReviewReducer, {
+    review: {
+      firstname: "",
+      lastname: "",
+      wineName: "",
+      photo: "",
+      producer: "",
+      percentage: "",
+      price: 0,
+      rating: 0,
+      foodPairing: "",
+      grape: "",
+      comment: "",
+    },
+  });
+
+  //therefore this state needs to go in the dispatch as well.
   const [name, setName] = useState<IName>({
     firstname: "",
     lastname: "",
   });
 
+  // this is only a state to keep track of the current booking information
   const [currentReview, setCurrentReview] = useState<IReviewContext>({
     currentReview: {
       firstname: "",
@@ -64,6 +89,7 @@ export const ReviewPage = () => {
     setIsFormVisible(false);
   };
 
+  console.log(createReview);
   console.log(currentReview);
 
   return (
@@ -73,21 +99,25 @@ export const ReviewPage = () => {
         <Link to="/">Go Back</Link>
       </button>
       <div></div>
-      <ReviewContext.Provider value={currentReview}>
-        {isFormVisible ? (
-          <>
-            <h2>
-              {"Welcome " +
-                currentReview.currentReview.firstname +
-                " please create a new Wine Review!"}
-            </h2>
-            <button onClick={handleChangeNameClick}>change name</button>
-            <Form />
-          </>
-        ) : (
-          <WelcomePage setName={handleNameChange} />
-        )}
-      </ReviewContext.Provider>
+      <ReviewReducerContext.Provider value={createReview}>
+        <ReviewDispatchContext.Provider value={dispatch}>
+          <ReviewContext.Provider value={currentReview}>
+            {isFormVisible ? (
+              <>
+                <h2>
+                  {"Welcome " +
+                    currentReview.currentReview.firstname +
+                    " please create a new Wine Review!"}
+                </h2>
+                <button onClick={handleChangeNameClick}>change name</button>
+                <Form />
+              </>
+            ) : (
+              <WelcomePage setName={handleNameChange} />
+            )}
+          </ReviewContext.Provider>
+        </ReviewDispatchContext.Provider>
+      </ReviewReducerContext.Provider>
     </>
   );
 };
